2e94788ce98969f9ab07b2f714d45532f7af4e63
Michael Vessia <michael@vessia.net>
2022-07-30 00:48:35 -0400

    Add tailwind and a basic footer

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
+import "tailwindcss/tailwind.css";
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
